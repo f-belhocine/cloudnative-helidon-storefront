07c6a537acea1b248ea02c6c72f80d2246a8fdbe
Farouk Belhocine <farouk.belhocine@hotmail.com>
2024-07-11 15:14:04 +0100

    Update StatusResource.java upgrade to version 1.0.0

--- helidon-storefront-full/src/main/java/com/oracle/labs/helidon/storefront/resources/StatusResource.java
+++ helidon-storefront-full/src/main/java/com/oracle/labs/helidon/storefront/resources/StatusResource.java
@@ -55,7 +55,7 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/status")
 @RequestScoped
 public class StatusResource {
-	public final static String VERSION = "0.0.1";
+	public final static String VERSION = "1.0.0";
 	private static final JsonBuilderFactory JSON = Json.createBuilderFactory(Collections.emptyMap());
 	private String storename = "Not set";
 	private static final SimpleDateFormat format = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss.SSS");
